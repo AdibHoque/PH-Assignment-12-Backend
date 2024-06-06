6b6347ca723bc9dcd8ed94006e726a20e5ff670a
AdibHoque <adibhoque04@gmail.com>
2024-06-06 21:20:46 +0600

    Added dotenv & Conncted MongoDB Server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express()
@@ -6,6 +7,20 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.knvnnno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("API server is running for TrueBond Matrimony!")
 })
@@ -13,3 +28,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Listening to Port: ", port)
 })
+
+async function run() {
+  try {
+    // // Connect the client to the server(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
