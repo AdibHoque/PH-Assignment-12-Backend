fb55c5939b3da7fd7a2909b99ca9cb846cf96e8c
AdibHoque <adibhoque04@gmail.com>
2024-06-10 00:42:14 +0600

    Added payment intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 app.use(cors())
 app.use(express.json())
@@ -36,6 +37,7 @@ async function run() {
     const biodataCollection = client.db('TrueBond').collection('biodatas');
     const premiumCollection = client.db('TrueBond').collection('premiumrequests');
     const usersCollection = client.db('TrueBond').collection('users');
+    const marriedCollection = client.db('TrueBond').collection('marriedstory');
 
     app.get("/biodatas", async (req, res) => {
       const idQuery = req.query.id
@@ -139,6 +141,61 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updatedDoc)
       res.send(result)
     })
+
+    app.patch('/biodatas/premium/:email', async (req, res) => {
+      const email = req.params.email
+      const filter = { contactEmail: email }
+      const updatedDoc = {
+        $set: {
+          premium: true
+        }
+      }
+      const result = await biodataCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.get("/marriedstory", async (req, res) => {
+      const cursor = marriedCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/stats", async (req, res) => {
+      const cursor = biodataCollection.find();
+      const result = await cursor.toArray();
+      const maleData = result.filter(d => d.gender == 'Male')
+      const femaleData = result.filter(d => d.gender == 'Female')
+
+      const cursor2 = marriedCollection.find();
+      const result2 = await cursor2.toArray();
+
+      const cursor3 = premiumCollection.find();
+      const result3 = await cursor3.toArray();
+
+      const data = {
+        totalBiodatas: result.length,
+        maleBiodatas: maleData.length,
+        femaleBiodatas: femaleData.length,
+        marriages: result2.length,
+        premiumUsers: result3.length
+      }
+      res.send(data)
+    })
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
